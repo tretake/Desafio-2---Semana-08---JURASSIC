1ebd3000e268b9f6abe0412b37d2063b4a94ab38
Caio Cavalcanti <caio-cavalcanti@hotmail.com>
2025-01-06 16:48:40 -0300

    Feat: Creation of header for all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 import './App.css'
 
 import { BrowserRouter,Routes,Route,Link } from 'react-router-dom'
@@ -17,7 +13,6 @@ import SignUp from './pages/SignUp'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
--- src/componenst/Header.tsx
+++ src/componenst/Header.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../assets/logo.png'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className="w-full bg-[#1E293B] flex space-betewn items-center justify-between p-4 max-h-[80px]"> 
+      <div className="flex items-center space-x-4"> 
+        <Link to="/">
+          <img src={Logo} alt="Logo" className="w-12 h-12 object-cover" /> 
+        </Link>
+        <h1 className="text-white text-xl font-bold">Project Manager</h1> 
+        </div>
+      <button>
+        {/* Botaozinho de teSte */}
+        Login
+      </button>
     </div>
+
+
+
+
+
+
+
+
   )
 }
 
