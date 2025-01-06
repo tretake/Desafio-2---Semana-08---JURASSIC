ee1b48c5f1a5306a0e444a22e5e847b4711fa34e
ruan <ruanlourencosilva.rj@gmail.com>
2025-01-06 14:41:46 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import './App.css'
+
+import { BrowserRouter,Routes,Route,Link } from 'react-router-dom'
+
+import Header from './componenst/Header'
+import Footer from './componenst/Footer'
+
+import Home from './pages/Home'
+import Kanban from './pages/Kanban'
+import Locked from './pages/Locked'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/kanban' element={<Kanban/>} />
+        <Route path='/login' element={<Login/>} />
+      </Routes>
+
+      <Footer/>
+    </BrowserRouter>
+  )
+}
+
+export default App
