b231f630cf44585db2e006add0717631ddb23c47
Tadeu <josetadeundasilva@gmail.com>
2025-01-07 12:32:11 -0300

    Incones_Footer_Link
    coloquei os no footer e os links tbm fiz uma pasta com umas img na pasta public

--- src/componenst/Footer.tsx
+++ src/componenst/Footer.tsx
@@ -8,15 +8,15 @@ const Footer: React.FC = () => {
     <div className="w-[136px] h-[34.46px] relative">
         <div className="w-[34px] h-[34px] left-0 top-0 absolute">
             <div className="w-[34px] h-[34px] left-0 top-0 absolute bg-white rounded-full shadow-[0px_4px_14px_0px_rgba(0,0,0,0.15)]" />
-            <div className="w-3 h-3 left-[11px] top-[11px] absolute overflow-hidden" />
+            <div className="w-3 h-3 left-[11px] top-[11px] absolute overflow-hidden" ><a href="https://www.facebook.com/?locale=pt_BR"target='_blank' ><img src="/public/images/facebook-1.jpg" alt="" /></a> </div> 
         </div>
         <div className="w-[34px] h-[34px] left-[50px] top-0 absolute">
             <div className="w-[34px] h-[34px] left-0 top-0 absolute bg-white rounded-full shadow-[0px_4px_14px_0px_rgba(0,0,0,0.15)]" />
-            <div className="w-[13px] h-[13px] left-[11px] top-[10px] absolute overflow-hidden" />
+            <div className="w-[13px] h-[13px] left-[11px] top-[10px] absolute overflow-hidden" ><a href="https://www.instagram.com/" target='_blank' ><img src="/public/images/instagram-1.jpg" alt="" /></a> </div>
         </div>
         <div className="w-[34px] h-[34px] left-[102px] top-[0.46px] absolute">
             <div className="w-[34px] h-[34px] left-0 top-0 absolute bg-white rounded-full shadow-[0px_4px_14px_0px_rgba(0,0,0,0.15)]" />
-            <div className="w-[11px] h-[11px] left-[11px] top-[11px] absolute overflow-hidden" />
+            <div className="w-[11px] h-[11px] left-[11px] top-[11px] absolute overflow-hidden" > <a href="https://www.linkedin.com/" target='_blank' ><img src="/public/images/linkedin-1.jpg" alt="" /></a></div>
         </div>
     </div>
 
