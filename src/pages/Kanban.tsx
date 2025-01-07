ee8b35e21b73e71e341e4e3dee4ed6cbe2be1be6
ruan <ruanlourencosilva.rj@gmail.com>
2025-01-07 18:02:49 -0300

    feat: settings descktop version done

--- src/pages/Kanban.tsx
+++ src/pages/Kanban.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Kanban = () => {
   return (
     <div>
-      
+a
+        <img className='w-full' src="./images/Draggable-area.png" alt="" />
+
+
+
     </div>
   )
 }
