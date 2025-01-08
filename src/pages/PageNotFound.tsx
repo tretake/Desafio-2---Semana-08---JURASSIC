cdc81453ffa65724e228797e60b4900db20f1299
Caio Cavalcanti <caio-cavalcanti@hotmail.com>
2025-01-08 15:04:38 -0300

    Feat: add metodo get

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
 
-const PageNotFound = () => {
+const PageNotFound = ({dados}) => {
+
+const { tasks, users } = dados
+console.log('minha props',dados)
   return (
     <div>
-      
-    </div>
-  )
-}
+    {tasks && tasks.length > 0 ? (
+        tasks.map((task) => (
+          <div key={task.id} style={{ border: "1px solid #ddd", margin: "10px", padding: "10px" }}>
+            <h2>{task.title}</h2>
+            <p><strong>Prioridade:</strong> {task.priority}</p>
+            <p><strong>Progresso:</strong> {task.progress}%</p>
+            <p><strong>Membros:</strong> {task.members.join(", ")}</p>
+          </div>
+        ))
+      ) : (
+        <p>Nenhuma tarefa encontrada.</p>
+      )}
+  </div>
+  );
+};
 
-export default PageNotFound
+export default PageNotFound;
