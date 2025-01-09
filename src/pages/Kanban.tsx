6a22c1f1c585db8408b8a9f59017e31f52a652f0
ruan <ruanlourencosilva.rj@gmail.com>
2025-01-09 15:09:03 -0300

    fix:overlaping bug fixed

--- src/pages/Kanban.tsx
+++ src/pages/Kanban.tsx
@@ -22,11 +22,15 @@ const Kanban = () => {
     const toDoColor = 'purple';
 
   return (
-    <div className=' flex relative m-10 justify-center items-center gap-10  ' >
+    <div className=' flex relative m-10    justify-center items-center gap-10 ' 
+    >
+      
+      <img className='absolute w-full  z-[-99]' src="./images/Kanban_background.png" alt="" />
+      
+      <button   className='bg-black  absolute top-[100px]'  > <svg xmlns="http://www.w3.org/2000/svg" width="12" height="20" viewBox="0 0 12 20" fill="none">
+<path d="M10 10V2H11V0H1V2H2V10L0 12V14H5.2V20H6.8V14H12V12L10 10Z" fill="white"/>
+</svg></button>
 
-      <img className='absolute  w-full z-[-99] ' src="./images/Kanban_background.png" alt="" />
-      
-      
       <KanbanCol  color={toDoColor} label='To do' number={25}>
         <KanbanCard priority='Low' label='TASK' color={toDoColor}/>
         <KanbanCard priority='Mid' label='TASK' color={toDoColor}/>
@@ -44,7 +48,6 @@ const Kanban = () => {
       <KanbanCard priority='Low' label='TASK' color={doneColor} image='./images/lens.jpg'/>
         <KanbanCard priority='Mid' label='TASK' color={doneColor}/>
       </KanbanCol>
-
 
 
 
