60e75aed2acadd59e54832d36d8e0ee9dfe277f3
DeividsonOmedio <deividsonomedio@outlook.com>
2025-01-09 22:51:09 -0300

    fix: conflitos resolvidos

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -63,7 +63,6 @@ const Button: React.FC<ButtonProps> = ({
   const sizeClasses = BUTTON_SIZES[screenSize][size];
 
   const classes = `px-4 py-2 h-12 ${kindClasses} ${sizeClasses}`;
-  const classes = `px-4 py-2 h-12 ${kindClasses} ${sizeClasses}`;
 
   return (
     <button className={classes} type={type} onClick={onClick}>
