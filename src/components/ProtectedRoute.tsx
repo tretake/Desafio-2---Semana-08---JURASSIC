cce53bfd8a334b33cfff4574c728c02ca3f061d4
Caio Cavalcanti <caio-cavalcanti@hotmail.com>
2025-01-12 03:49:50 -0300

    Feat: adicionando spinner

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,12 +1,24 @@
 import { useAuth } from "@clerk/clerk-react";
 import { Navigate } from "react-router-dom";
+import { Vortex } from 'react-loader-spinner'
+
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   const { isLoaded, isSignedIn } = useAuth();
 
   // Mostra um indicador de carregamento enquanto Clerk verifica a autenticação
   if (!isLoaded) {
-    return <div>Carregando...</div>;
+    return <div className="flex justify-center items-center h-100 w-full">
+     <Vortex
+  visible={true}
+  height="80"
+  width="80"
+  ariaLabel="vortex-loading"
+  wrapperStyle={{}}
+  wrapperClass="vortex-wrapper"
+  colors={['red', 'green', 'blue', 'yellow', 'orange', 'purple']}
+  />
+      </div>;
   }
 
   // Redireciona para a página 403 se o usuário não estiver logado
